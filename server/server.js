b863c747fc438dee5bfe04d583ac872f8696a504
Nandana S <nandanas810@gmail.com>
2026-07-22 23:30:19 +0530

    Day 3: Setup backend structure and connect MongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
 
 const PORT = 5000;
@@ -7,7 +7,7 @@ const PORT = 5000;
 app.get("/", (req, res) => {
     res.send("SkillBridge Backend Running Successfully 🚀");
 });
-
+connectDB();
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
