7c2035eca6e83c3d6ee34eed45900239c0ab50b4
Nandana S <nandanas810@gmail.com>
2026-08-03 21:51:14 +0530

    Day 6: Create User Login API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,8 @@
 const User = require("../models/userModel");
 
+// =======================
+// Register User
+// =======================
 const registerUser = async (req, res) => {
 
     const { name, email, password } = req.body;
@@ -13,6 +16,38 @@ const registerUser = async (req, res) => {
     res.status(201).json(user);
 };
 
+// =======================
+// Login User
+// =======================
+const loginUser = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    // Check if user exists
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    // Check password
+    if (user.password !== password) {
+        return res.status(401).json({
+            message: "Invalid Password"
+        });
+    }
+
+    // Login Successful
+    res.status(200).json({
+        message: "Login Successful",
+        user
+    });
+};
+
 module.exports = {
     registerUser,
+    loginUser,
 };
